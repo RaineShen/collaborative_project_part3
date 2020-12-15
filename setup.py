9162600e381bf31d36a307f7737eb62acbe1aa7f
Yuxuan Cui <Me222CYX>
2020-12-14 20:48:29 -0800

    new

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
-name='Fittness_UBCO',
-version='0.1', 
+name='Fittness',
+version='1.0', 
 packages=find_packages(exclude=['tests*']), 
 license='MIT',
 description="A test python package",
