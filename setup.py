d6fa323fd45a1ffe75b04d9a6d0dfd37038cef74
Yuxuan Cui <Me222CYX>
2020-12-17 22:34:45 -0800

    new

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 setup(
 name='Fittness',
-version='1.0', 
+version='1.1', 
 packages=find_packages(exclude=['tests*']), 
 license='MIT',
 description="A test python package",
+install_requires=['bokeh','pygal'],
 url='https://github.com/RaineShen/data533Lab4.git', 
 author='YuxuanIsme,raineShen',
 author_email='cuiyuuxuan@gmail.com,rain.ya1213@gmail.com'
